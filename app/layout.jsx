b27b05f36751e97f4963e9aadb45bd6c21969558
Ashish Sigdel <a.asis.sigdel01@gmail.com>
2023-12-15 23:32:51 +0545

    Adding Navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import Nav from "@components/Nav";
 import "@styles/globals.css";
 
 export const metadata = {
@@ -12,7 +13,10 @@ const RootLayout = ({ children }) => {
         <div className="main">
           <div className="gradient" />
         </div>
-        <main className="app">{children}</main>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
       </body>
     </html>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import Feed from "@components/Feed";
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        IdeaForge is an open-source AI prompting tool for modern world to
+        discover, create and share creative prompts.{" "}
+      </p>
 
-export default Home
+      <Feed />
+    </section>
+  );
+};
+
+export default Home;
